f2f316dbbbf44ab5d13c9db8dceeaecbab0a45f8
Oleksandr Myronchuk <smironcuk35@gmail.com>
2022-10-10 18:47:31 +0300

    created react-sneakers project

---
+++ src/App.js
@@ -0,0 +1,174 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import axios from "axios";
+import Drawer from "./components/Drawer";
+import Header from "./components/Header";
+import AppContext from "./context";
+
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import Orders from "./pages/Orders";
+
+function App() {
+  const [items, setItems] = React.useState([]);
+  const [cartItems, setCartItems] = React.useState([]);
+  const [favorites, setFavorites] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState("");
+  const [cartOpened, setCartOpened] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      try {
+        const [cartResponse, favoriteResponse, itemsResponse] =
+          await Promise.all([
+            axios.get("https://6342eb673f83935a784c66f4.mockapi.io/cart"),
+            axios.get("https://61de0dabf60e8f0017668ba7.mockapi.io/favorites"),
+            axios.get("https://61de0dabf60e8f0017668ba7.mockapi.io/items"),
+          ]);
+
+        setIsLoading(false);
+
+        setCartItems(cartResponse.data);
+        setFavorites(favoriteResponse.data);
+        setItems(itemsResponse.data);
+      } catch (error) {
+        alert("Ошибка при запросе данных ;(')");
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const onAddToCart = async (obj) => {
+    try {
+      const findItem = cartItems.find(
+        (item) => Number(item.parentId) === Number(obj.id)
+      );
+      if (findItem) {
+        setCartItems((prev) =>
+          prev.filter((item) => Number(item.parentId) !== Number(obj.id))
+        );
+        await axios.delete(
+          `https://61de0dabf60e8f0017668ba7.mockapi.io/cart/${findItem.id}`
+        );
+      } else {
+        setCartItems((prev) => [...prev, obj]);
+        const { data } = await axios.post(
+          "https://61de0dabf60e8f0017668ba7.mockapi.io/cart",
+          obj
+        );
+        setCartItems((prev) =>
+          prev.map((item) => {
+            if (item.parentId === data.parentId) {
+              return {
+                ...item,
+                id: data.id,
+              };
+            }
+            return item;
+          })
+        );
+      }
+    } catch (error) {
+      alert("Ошибка при добавлении в корзину");
+      console.error(error);
+    }
+  };
+
+  const onRemoveItem = (id) => {
+    try {
+      axios.delete(`https://61de0dabf60e8f0017668ba7.mockapi.io/cart/${id}`);
+      setCartItems((prev) =>
+        prev.filter((item) => Number(item.id) !== Number(id))
+      );
+    } catch (error) {
+      alert("Ошибка при удалении из корзины");
+      console.error(error);
+    }
+  };
+
+  const onAddToFavorite = async (obj) => {
+    try {
+      if (favorites.find((favObj) => Number(favObj.id) !== Number(obj.id))) {
+        axios.delete(
+          `https://61de0dabf60e8f0017668ba7.mockapi.io/favorites/${obj.id}`
+        );
+        setFavorites((prev) =>
+          prev.filter((item) => Number(item.id) !== Number(obj.id))
+        );
+      } else {
+        const { data } = await axios.post(
+          "https://61de0dabf60e8f0017668ba7.mockapi.io/favorites",
+          obj
+        );
+        setFavorites((prev) => [...prev, data]);
+      }
+    } catch (error) {
+      alert("Не удалось добавить в избранные");
+      console.error(error);
+    }
+  };
+
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const isItemAdded = (id) => {
+    return cartItems.some((obj) => Number(obj.parentId) === Number(id));
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        items,
+        cartItems,
+        favorites,
+        isItemAdded,
+        onAddToFavorite,
+        onAddToCart,
+        setCartOpened,
+        setCartItems,
+      }}
+    >
+      <div className="wrapper clear">
+        <Drawer
+          items={cartItems}
+          onClose={() => setCartOpened(false)}
+          onRemove={onRemoveItem}
+          opened={cartOpened}
+        />
+        <Header onClickCart={() => setCartOpened(true)} />
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                items={items}
+                cartItems={cartItems}
+                searchValue={searchValue}
+                setSearchValue={setSearchValue}
+                onChangeSearchInput={onChangeSearchInput}
+                onAddToFavorite={onAddToFavorite}
+                onAddToCart={onAddToCart}
+                isLoading={isLoading}
+              ></Home>
+            }
+            exact
+          ></Route>
+
+          <Route
+            path="/favorites"
+            element={<Favorites></Favorites>}
+            exact
+          ></Route>
+
+          <Route path="/orders" element={<Orders />} exact></Route>
+        </Routes>
+      </div>
+    </AppContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/pages/Orders.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import axios from "axios";
+
+import Card from "../components/Card";
+
+function Orders() {
+  const [orders, setOrders] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get(
+          "https://61de0dabf60e8f0017668ba7.mockapi.io/orders"
+        );
+        // console.log(data.map((obj) => obj.items).flat());
+        // console.log(data.reduce((prev, obj) => [...prev, ...obj.items], []));
+        setOrders(data.reduce((prev, obj) => [...prev, ...obj.items], []));
+        setIsLoading(false);
+      } catch (error) {
+        alert("Ошибка при запросе заказов");
+        console.error(error);
+      }
+    })();
+  }, []);
+  return (
+    <div className="content p-40">
+      <div className="d-flex align-center justify-between mb-40">
+        <h1>Мои заказы </h1>
+      </div>
+
+      <div className="d-flex flex-wrap">
+        {(isLoading ? [...Array(8)] : orders).map((item, index) => (
+          <Card key={index} loading={isLoading} {...item} />
+        ))}
+      </div>
+    </div>
+  );
+}
+export default Orders;
